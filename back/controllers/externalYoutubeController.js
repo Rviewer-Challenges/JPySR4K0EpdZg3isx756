60ed1ffe5e356ee4437c7bb697c8db97428a9606
alex <lxalonso@gmail.com>
2022-09-08 12:10:37 +0200

    git conflict ok

---
+++ back/controllers/externalYoutubeController.js
@@ -0,0 +1,37 @@
+const externalGitService = require('../services/externalGitServices');
+const developersServices = require('../services/developersServices')
+
+const updateDevelopersYoutube = async (request, response, next) => {
+    try {
+        const developers = await developersServices.getAllDevelopers();
+//Acutalización repositorios y datos usuario de git
+        for (dev of developers) {
+            const newDev = { gitHub: await externalGitService.getGitUserDetails(dev.gitHub.gitUser) };
+            newDev.gitHub.repositories = await externalGitService.getGitRepos(dev.gitHub.gitUser);
+            dev.gitHub = newDev.gitHub;
+            await developersServices.updateDeveloper(dev);
+
+        }
+
+        response
+            .status(200)
+            .send({
+                data:
+                {
+                    developers: developers,
+                }
+            })
+    } catch (error) {
+        response
+            .status(error.status || 500)
+            .send({
+                satus: "FAILED",
+                data: error.message,
+                code: error?.status || 500
+            })
+    }
+}
+
+module.exports = {
+    updateDevelopersYoutube
+}
